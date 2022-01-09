d43ba8f0773525e791a3aacdec8d6820c38ec27a
Pg1606 <pg16061999@gmail.com>
2022-01-09 20:34:04 +0530

    broken our homepage into smaller components, finished styling out menu-item component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
-import './homepage.component';
-import HomePage from './homepage.component';
+
+import HomePage from './pages/homepage/homepage.component.jsx';
 
 function App() {
   return (
